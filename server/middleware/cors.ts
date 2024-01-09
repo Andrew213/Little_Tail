0fa845b6d578d04d2277ecf4372a8f4b3656a353
Andrey Kochanov <kochanov@lanbilling.ru>
2024-01-09 16:44:15 +0300

    Сделал авторизацию через jwt токен

--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -4,7 +4,7 @@ const cors = () => {
     return (req: Request, res: Response, next: NextFunction) => {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE');
-        res.header('Access-Control-Allow-Headers', 'Content-Type');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         next();
     };
 };
